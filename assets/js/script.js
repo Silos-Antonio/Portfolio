d432aea1976cf28a13eb53aa89e932b0eecd8973
Antonio Silos <silosantonio95@gmail.com>
2025-12-19 01:50:49 -0300

    Resolve merge conflicts in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function() {
     const navLink = document.querySelectorAll('nav ul li a');
     const allArticles = document.querySelectorAll('main article');
 
+    // Esconde todos os artigos, exceto o primeiro, ao carregar a página
     allArticles.forEach ((article, index) => {
         if (index !== 0) {
             article.classList.add('oculto');
         }
     });
 
+    // Lógica de navegação entre as abas
     navLink.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
@@ -25,51 +26,17 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+    // Lógica de expansão dos cards de habilidades
     const headers = document.querySelectorAll('.cabecalho-card');
-    
     
     headers.forEach(header => {
         header.addEventListener('click', () => {
-            
             const parentCard = header.closest('.habilidades-conteudo');
-            
             const descriptionContainer = parentCard.querySelector('.habilidades-desc-conteudo');
-            
             const icon = header.querySelector('.ph-caret-down');
             
             descriptionContainer.classList.toggle('expandido');
-            
             icon.classList.toggle('rotacionar-icone');
         });
     });
 });
-
-=======
-document.addEventListener('DOMContentLoaded', function() {
-    const navLink = document.querySelectorAll('nav ul li a');
-    const allArticles = document.querySelectorAll('main article');
-
-    allArticles.forEach ((article, index) => {
-        if (index !== 0) {
-            article.classList.add('oculto');
-        }
-    });
-
-    navLink.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-
-            const targetId = this.getAttribute('href');
-            allArticles.forEach(article => {
-                article.classList.add('oculto');
-            });
-
-            const targetArticle = document.querySelector(targetId);
-            if (targetArticle) {
-                targetArticle.classList.remove('oculto');
-            }
-        });
-    });
-});
-
->>>>>>> bde066b550b1d9bb185c977be35ccc6afc385e68
