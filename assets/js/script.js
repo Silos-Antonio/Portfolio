20aea7d89275661dcecc8a7e9f189ce76bb69da4
Silos-Antonio <silosantonio95@gmail.com>
2026-02-21 23:12:38 -0300

    feat: adicionar interatividade e estilo às certificações, atualizar informações de projetos, adicionar diploma

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,3 +129,32 @@ document.addEventListener('DOMContentLoaded', function() {
         btnContatos.querySelector('i').classList.toggle('rotacionar-icone');
     });
 });
+
+const container = document.querySelector('.lista-certificacoes');
+
+let isDown = false;
+let startX;
+let scrollLeft; 
+
+container.addEventListener('mousedown', (e) => {
+    isDown = true;
+    container.classList.add('active');
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+});
+
+container.addEventListener('mouseleave', () => {
+    isDown = false;
+});
+
+container.addEventListener('mouseup', () => {
+    isDown = false;
+});
+
+container.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const walk = (x - startX) * 1.5;
+    container.scrollLeft = scrollLeft - walk;
+});
